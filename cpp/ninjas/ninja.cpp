4717ca406c2f287a995d9b8720b9d96328a39c9d
rxzheng <richardxzheng@outlook.com>
2024-01-07 04:45:58 +0000

    tried ninja

--- cpp/ninjas/ninja.cpp
+++ cpp/ninjas/ninja.cpp
@@ -1,6 +1,23 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <cmath>
 using namespace std;
 int main(){
  ifstream inputFile("ninjain.txt");
  ofstream outputFile("ninjaout.txt");
+  
+ int a, b, n, k;
+ inputFile >> n;
+ inputFile >> k;
+ 
+ a = k * floor(n / (k + 1));
+ b = (n % (k+1)) - 1;
+  cout << "a: " << a;
+  cout << "b: " << b;
+if(k == 0){
+  outputFile << 0;
+}else{
+ outputFile << a + b;
+}
 }

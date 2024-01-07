a46a95338da478cd2ffebbe8b90a67da8bec334a
rxzheng <richardxzheng@outlook.com>
2024-01-07 13:25:42 +1100

    made friendlist

--- cpp/encyclopedia/ency.cpp
+++ cpp/encyclopedia/ency.cpp
@@ -1,11 +1,19 @@
-#include <iostream>
-#include <fstream>
-#include <map>
-#include <climits>
-#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
 int main(){
   ifstream inputFile("encyin.txt");
   ofstream outputFile("encyout.txt");
   
-
+  int pages, questions;
+  map<int, int> mp;
+  
+  inputFile >> pages >> questions;
+  for(int i = 1, a; i < pages + 1; i++){
+    inputFile >> a;
+    mp[i] = a;
+  }
+  for(int i = 0, a; i < questions; i++){
+    inputFile >> a;
+    outputFile << mp[a] << "\n";
+  }
 }
